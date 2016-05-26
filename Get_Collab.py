c7d0a98fefe8ae96ab8847237f0eebc7e3cc129d
Samuel Roberts <sproberts92@outlook.com>
2016-05-26 17:09:10 +0200

    Add __main__ check and main() function

--- Get_Collab.py
+++ Get_Collab.py
@@ -3,28 +3,32 @@ import os
 import datetime
 import time
 
-filename = 'Authors'
+def main():
+	filename = 'Authors'
 
-arXiv_url = 'http://export.arxiv.org/api/query?search_query=au:AUTHOR&id_list=&start=0&max_results=10000'
+	arXiv_url = 'http://export.arxiv.org/api/query?search_query=au:AUTHOR&id_list=&start=0&max_results=10000'
 
-if os.path.exists(filename + '_Out.txt'): os.remove(filename + '_Out.txt')
+	if os.path.exists(filename + '_Out.txt'): os.remove(filename + '_Out.txt')
 
-file_in = open('Input/' + filename + '_In.txt', 'r')
-file_out = open('Output/' + filename + '_Out.txt', 'w')
+	file_in = open('Input/' + filename + '_In.txt', 'r')
+	file_out = open('Output/' + filename + '_Out.txt', 'w')
 
-author_list = file_in.readlines()
+	author_list = file_in.readlines()
 
-for author in author_list:
-	print(author.strip().replace('_', ', '))
-	print(author.strip().replace('_', ', '), end = '\n', file = file_out)
-	author_feed = feedparser.parse(arXiv_url.replace('AUTHOR', author.rstrip()))
+	for author in author_list:
+		print(author.strip().replace('_', ', '))
+		print(author.strip().replace('_', ', '), end = '\n', file = file_out)
+		author_feed = feedparser.parse(arXiv_url.replace('AUTHOR', author.rstrip()))
 
-	i = 0
-	for author_paper in author_feed.entries:
-		i += 1
-		count = str(i)
-		print(count + ' ' + author_paper.title)
-		for coauthor in author_paper.authors:
-	 		name = coauthor.name
-	 		print(name, end = '\n', file = file_out)
-		time.sleep(3)
+		i = 0
+		for author_paper in author_feed.entries:
+			i += 1
+			count = str(i)
+			print(count + ' ' + author_paper.title)
+			for coauthor in author_paper.authors:
+		 		name = coauthor.name
+		 		print(name, end = '\n', file = file_out)
+			time.sleep(3)
+
+if __name__ == '__main__':
+	main()
