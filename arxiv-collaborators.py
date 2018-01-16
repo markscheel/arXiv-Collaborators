0994dd95b324ce95c59eda954b6260981dcd371a
Samuel Roberts <sproberts92@outlook.com>
2018-01-16 22:46:40 +0100

    Some stylistic improvements

--- arxiv-collaborators.py
+++ arxiv-collaborators.py
@@ -1,50 +1,45 @@
-import feedparser as fp
-from datetime import datetime as dt
 import time
+from datetime import datetime as dt
+from collections import defaultdict
+
+import feedparser as fp
 
 arXiv_url = 'http://export.arxiv.org/api/query?search_query=au:{0}&id_list=&start=0&max_results=10000'
+input_path = 'Input'
+output_path = 'Output'
+
 
 def main():
-	author_list = open('Input/Authors.dat', 'r').readlines()
+	author_list = open(f'{input_path}/Authors.dat', 'r').readlines()
 
 	date_time = dt.now().strftime('%Y_%M_%d_%H_%M_%S')
 
 	for author in author_list:
-
-		# Open a unique file for this author
-		file_out = open('Output/{0}_{1}.dat'.format(date_time, author.strip()), 'w')
-
 		# Get the data from arXiv
-		author_feed = fp.parse(arXiv_url.format(author.rstrip()))
+		author = author.strip()
+		feed = fp.parse(arXiv_url.format(author))
 
 		# Create an empty dict to store counts of the coauthors
-		coauthors = {}
+		coauthors = defaultdict(int)
 
-		count = 0
-		total = len(author_feed.entries)
+		total = len(feed.entries)
 
-		for author_paper in author_feed.entries:
+		for i, paper in enumerate(feed.entries):
+			title = paper.title.encode('utf-8')
+			print(f'{i + 1}/{total}: {title}')
 
-			# Print out which paper we're up to so we can keep track as its running.
-			count += 1
-			print('{0}/{1}: {2}'.format(count, total, author_paper.title.encode('utf-8')))
+			for coauthor in paper.authors:
+	 			coauthors[coauthor.name] += 1
 
-			for coauthor in author_paper.authors:
-		 		# Try to increase the count in the dict, if an exception is
-		 		# raised the key must not exist yet, so create it.
-		 		try:
-		 			coauthors[coauthor.name] += 1
-		 		except KeyError:
-		 			coauthors[coauthor.name] = 1
-
-		# Iterate over the dict and write the information to file.
-		for c in coauthors:
-			print('{0}, {1}'.format(c, coauthors[c]), file=file_out)
-
-		file_out.close()
+		# Open a unique file for this author
+		f_name = f'{output_path}/{date_time}_{author}.dat'
+		with open(f_name, 'w') as fp_out:
+			for name, count in coauthors.items():
+				print(f'{name}, {count}', file=fp_out)
 
 	# Limit the query rate to be nice to the arXiv servers.
 	time.sleep(3)
+
 
 if __name__ == '__main__':
 	main()
